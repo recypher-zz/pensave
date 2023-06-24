3b37b8898a29e63a36da7f8e07baa59500d9f6c8
recypher-zz <kniemerg001@me.com>
2023-06-24 01:56:01 -0400

    Pushing most recent updates, created progress bar system for backup and working to deploy jszip as well

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const { app, BrowserWindow } = require('electron');
+const { app, dialog, BrowserWindow, ipcMain } = require('electron');
 const fs = require('fs');
 const path = require('path');
+const os = require('os');
+const ProgressBar = require('electron-progressbar');
+const JSzip = require('jszip');
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -17,10 +20,76 @@ const createWindow = () => {
     win.loadFile(path.join(__dirname, 'renderer/index.html'));
 }
 
-app.whenReady().then(() => {
-    createWindow();
-})
+//Function to create a zip file with progress bar
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+async function zipProgressBar(filePaths, zipPath) {
+    const zip = new JSzip();
+    const totalFiles = filePaths.length;
+
+    // Create a progress bar with total number of files
+    const progressBar = new ProgressBar({
+        text: 'Creating backup...',
+        detail: 'Processing files',
+        browserWindow:{
+            webPreferences: {
+                nodeIntegration: true
+            },
+            title: 'Backup Progress',
+            closable: false,
+            minimizable: false,
+            maximizable: false,
+            width: 400,
+            height: 150,
+            autoHideMenuBar: true
+        }
+    });
+
+    function updateProgressBar(currentFile) {
+        const progress = (currentFile / totalFiles) * 100;
+        progressBar.value = progress;
+        progressBar.detail = `Processing ${currentFile + 1} of ${totalFiles}`
+    }
+
+    //Read each file and add it to the zip
+    for (let i = 0; i < totalFiles; i++) {
+        const filePath = filePaths[i];
+        const fileData = await fs.promises.readFile(filePath);
+        zip.file(filePath, fileData);
+        updateProgressBar(i);
+    }
+
+    const content = await zip.generateAsync({ type: 'nodebuffer' });
+
+    // Write the zip file to disk
+    await fs.promises.writeFile(zipPath, content);
+
+    // Close the progress bar window
+
+    progressBar.close();
+}
+
+// Example usage
+const filesToBackup = [
+    ''
+]
+
+
+// Check Operating System being ran currently
+function isMacOS() {
+    return os.platform() === 'darwin';
+}
+
+app.whenReady().then(() => {
+    //Check OS on app startup
+    if (isMacOS()) {
+        console.log('Is Mac, killing process');
+        //Display an error dialog if running on macOS
+        dialog.showErrorBox(
+            'Unsupported Operating System',
+            'This application is currently not support on macOS'
+        );
+        app.quit();
+    }
+    console.log('Not a mac, starting.');
+    createWindow();
 })
