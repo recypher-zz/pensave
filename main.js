866e5140295d0d1dbae2930f469e79e89493b603
recypher-zz <kniemerg001@me.com>
2023-06-26 11:09:17 -0400

    Function to backup file based off of %appdata% file pathing working, need to find a way of doing a file check to ensure that the files even exist before we do the backup and allow users to select their FFXIV Mods dir as well

--- main.js
+++ main.js
@@ -9,69 +9,92 @@ const createWindow = () => {
     const win = new BrowserWindow({
         autoHideMenuBar: true,
         width: 800,
-        height: 600,
+        height: 500,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: true,
             preload: path.join(__dirname, 'preload.js')
-        }
+        },
+        resizable: false
     })
 
     win.loadFile(path.join(__dirname, 'renderer/index.html'));
 }
+
+// %appdata% file path
+const appDataPath = app.getPath('appData');
+console.log(appDataPath);
 
 //Function to create a zip file with progress bar
 
 async function zipProgressBar(filePaths, zipPath) {
     const zip = new JSzip();
     const totalFiles = filePaths.length;
-
+  
     // Create a progress bar with total number of files
     const progressBar = new ProgressBar({
-        text: 'Creating backup...',
-        detail: 'Processing files',
-        browserWindow:{
-            webPreferences: {
-                nodeIntegration: true
-            },
-            title: 'Backup Progress',
-            closable: false,
-            minimizable: false,
-            maximizable: false,
-            width: 400,
-            height: 150,
-            autoHideMenuBar: true
-        }
+      text: 'Creating backup...',
+      detail: 'Processing files',
+      browserWindow: {
+        webPreferences: {
+          nodeIntegration: true
+        },
+        title: 'Backup Progress',
+        closable: false,
+        minimizable: false,
+        maximizable: false,
+        width: 400,
+        height: 150,
+        autoHideMenuBar: true
+      }
     });
-
+  
     function updateProgressBar(currentFile) {
-        const progress = (currentFile / totalFiles) * 100;
-        progressBar.value = progress;
-        progressBar.detail = `Processing ${currentFile + 1} of ${totalFiles}`
+      progressBar.detail = `Processing file ${currentFile + 1} of ${totalFiles}`;
     }
-
-    //Read each file and add it to the zip
-    for (let i = 0; i < totalFiles; i++) {
-        const filePath = filePaths[i];
+  
+    async function addFileToZip(filePath, basePath) {
+      const stats = await fs.promises.stat(filePath);
+  
+      if (stats.isDirectory()) {
+        // Add directory to the zip
+        const folderPath = path.relative(basePath, filePath);
+  
+        // Read the directory and add its contents recursively
+        const files = await fs.promises.readdir(filePath);
+        for (const file of files) {
+          const subFilePath = path.join(filePath, file);
+          await addFileToZip(subFilePath, basePath);
+        }
+      } else {
+        // Add file to the zip with relative directory structure
         const fileData = await fs.promises.readFile(filePath);
-        zip.file(filePath, fileData);
-        updateProgressBar(i);
+        const relativePath = path.relative(basePath, filePath);
+        zip.file(relativePath, fileData);
+      }
+  
+      updateProgressBar(filePaths.indexOf(filePath));
     }
-
+  
+    for (let i = 0; i < totalFiles; i++) {
+      const filePath = filePaths[i];
+      await addFileToZip(filePath, appDataPath);
+    }
+  
     const content = await zip.generateAsync({ type: 'nodebuffer' });
-
-    // Write the zip file to disk
     await fs.promises.writeFile(zipPath, content);
-
-    // Close the progress bar window
-
+  
     progressBar.close();
-}
+  }
 
-// Example usage
+// Example usage - Writing a test to function off of. 
 const filesToBackup = [
-    ''
+    `${appDataPath}/XIVLauncher/backups/Penumbra`,
+    `${appDataPath}/XIVLauncher/pluginConfigs/Penumbra.json`,
+    `${appDataPath}/XIVLauncher/pluginConfigs/Penumbra`
 ]
+
+const backupZipPath = 'C:/Users/kniem/Desktop/pensave.zip';
 
 
 // Check Operating System being ran currently
@@ -92,4 +115,11 @@ app.whenReady().then(() => {
     }
     console.log('Not a mac, starting.');
     createWindow();
+    // zipProgressBar(filesToBackup, backupZipPath)
+    //     .then(() => {
+    //         console.log('Backup created successfully!');
+    //     })
+    //     .catch((error) => {
+    //         console.error('Failed to create backup:', error);
+    //     })
 })
